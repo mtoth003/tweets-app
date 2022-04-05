07aa072b14686704c5c6c5a96a9b0bcf7321b258
Michael Toth <mtoth003@gmail.com>
2022-04-04 20:22:44 -0400

    updated the navbar added session creation and user authentication

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,16 @@
 Rails.application.routes.draw do
   get "/about", to: "about#index"
 
+  get "/password", to: "passwords#edit", as: :edit_password
+  patch "/password", to: "passwords#update"
+
+  get "/sign_up", to: "registrations#new"
+  post "/sign_up", to: "registrations#create"
+
+  get "/sign_in", to: "sessions#new"
+  post "/sign_in", to: "sessions#create"
+
+  delete "/logout", to: "sessions#destroy"
+
   root to: "main#index"
 end
